2a840afda54d0c170376e14d619e2a77fde4fb46
vegaronel <ronelvega31@gmail.com>
2025-02-16 15:04:29 +0800

    remove ppr experiment

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import Search from "@/app/ui/search";
 
 function Page() {
-  return (
-    <div>
-      Customers Page
-      <Search placeholder="Search for Customers" />
-    </div>
-  );
+  return <div>Customers Page</div>;
 }
 
 export default Page;
